112da61851353af6e6999d75ee10515443c30516
lidkapro <44070274+lidkapro@users.noreply.github.com>
2019-06-24 23:56:51 +0300

    webpack is ready

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import styled from 'styled-components'
+
+const Button = styled.button``
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Button>click</Button>
+            </div>
+        )
+    }
+}
+
+export default App
